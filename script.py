ee8671733bbf6e4799df29804cd19f32bfceea83
Vladislav Levchenko <qumangames@yandex.ru>
2025-09-06 11:26:52 +0300

    init project with script.py

---
+++ script.py
@@ -0,0 +1,22 @@
+
+age = 22
+height = 1.72
+name = "Vlad"
+message = "ошибка формирования обратитесь к служебному специалисту"
+
+while True:
+    try:
+        ticket = int(input("Введите 1 если есть билет, 0 если нет: "))
+
+        if ticket == 1:
+            print(age)
+            print(height)
+            print(name)
+            break  # выходим из цикла
+        elif ticket == 0:
+            print(message)
+            break  # тоже выходим
+        else:
+            print("Ошибка: допустимы только значения 0 или 1")
+    except ValueError:
+        print("Ошибка: нужно вводить число, а не текст")
